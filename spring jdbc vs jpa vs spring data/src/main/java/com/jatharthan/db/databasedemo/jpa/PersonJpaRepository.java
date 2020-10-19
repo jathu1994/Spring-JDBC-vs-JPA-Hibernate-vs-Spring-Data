d62e40a0b3bdf280801aaa93d0f66a9467d9fdec
jatharthan <jathu10061994@gmail.com>
2020-10-19 17:03:28 +0530

    jpa and spring data edits

---
+++ spring jdbc vs jpa vs spring data/src/main/java/com/jatharthan/db/databasedemo/jpa/PersonJpaRepository.java
@@ -0,0 +1,38 @@
+package com.jatharthan.db.databasedemo.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.jatharthan.db.databasedemo.entity.Person;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+	
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public List<Person> findAll() {
+		TypedQuery<Person> namedQuery = entityManager.createNamedQuery("find_all_persons",Person.class);
+		return namedQuery.getResultList();
+	}
+	
+	public Person findById(int id) {
+		return entityManager.find(Person.class, id);
+	}
+	
+	public Person update(Person person) {
+		return entityManager.merge(person);
+	}
+	
+	public Person insert(Person person) {
+		return entityManager.merge(person);
+	}
+
+}
